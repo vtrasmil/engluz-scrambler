7d8bd72fbbf665ce6b36dad7adbbaf95ef5250a4
Van Trasmil <vtrasmil@gmail.com>
2025-03-31 00:22:05 +0800

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  basePath: '/scrambled',
+  // basePath: '/scrambled',
   images: {
     loader: "custom"
   }
